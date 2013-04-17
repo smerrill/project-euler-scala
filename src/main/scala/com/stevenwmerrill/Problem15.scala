7ea62578aa171cfbc864547cf59017bfa973b866
Steven Merrill <steven.merrill@gmail.com>
2013-04-17 13:31:07 -0400

    Refactor 15 into just an App object.

--- src/main/scala/com/stevenwmerrill/Problem15.scala
+++ src/main/scala/com/stevenwmerrill/Problem15.scala
@@ -2,15 +2,9 @@ package com.stevenwmerrill
 
 /**
  */
-class Problem15 {
+object Problem15 extends App {
   implicit def intToBigInt(n: Int) = BigInt(n)
   def factorial(x: BigInt): BigInt = (1 to x).reduceLeft(_ * _)
   def paths(x: BigInt): BigInt = factorial(2 * x) / (factorial(x) * factorial(x))
   println(paths(20))
-}
-
-object Problem15 {
-  def main(args: Array[String]) {
-    new Problem15
-  }
 }
