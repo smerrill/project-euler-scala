d594e115a9baa25c2403b63ba552c9d6c59d0702
Steven Merrill <steven.merrill@gmail.com>
2013-04-17 13:31:22 -0400

    Problem 16 solution.

---
+++ src/main/scala/com/stevenwmerrill/Problem16.scala
@@ -0,0 +1,11 @@
+package com.stevenwmerrill
+
+/**
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
+ */
+object Problem16 extends App {
+  println(
+    BigInt(2).pow(1000).toString.foldLeft(0)(_ + _.toString.toInt)
+  )
+}
